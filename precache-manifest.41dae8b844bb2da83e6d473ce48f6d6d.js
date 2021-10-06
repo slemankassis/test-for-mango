be794bd4a7273a39ce300d187c792db8131f3150
Sleman Kassis <slemankassis@hotmail.com>
2021-10-06 23:41:42 +0100

    Updates

---
+++ precache-manifest.41dae8b844bb2da83e6d473ce48f6d6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e55356c67cd022baac80d8b300885f5",
+    "url": "/test-for-mango/index.html"
+  },
+  {
+    "revision": "f8afda2e3596e385f9d5",
+    "url": "/test-for-mango/static/css/main.8914190a.chunk.css"
+  },
+  {
+    "revision": "25936d3a7ca8a98158ea",
+    "url": "/test-for-mango/static/js/2.28907e98.chunk.js"
+  },
+  {
+    "revision": "850e1eefc965dc3ece19d356f8d7388f",
+    "url": "/test-for-mango/static/js/2.28907e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a41d0bf30a9ff7579ec",
+    "url": "/test-for-mango/static/js/3.7673659d.chunk.js"
+  },
+  {
+    "revision": "f8afda2e3596e385f9d5",
+    "url": "/test-for-mango/static/js/main.0328ee8e.chunk.js"
+  },
+  {
+    "revision": "93ac6dc22daf131660ad",
+    "url": "/test-for-mango/static/js/runtime-main.c638598e.js"
+  }
+]);
