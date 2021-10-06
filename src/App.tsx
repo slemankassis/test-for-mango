579c6edc99e2cea8c94d80d47139641f5af6c0dd
Sleman Kassis <slemankassis@hotmail.com>
2021-10-06 18:52:24 +0100

    Lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { hot } from 'react-hot-loader';
 import { store } from './store/store';
 import { Provider } from 'react-redux';
-import Exercise1 from './exercises/components/Exercise1';
-import Exercise2 from './exercises/components/Exercise2';
+import Exercise2 from './demo/components/exercise2/Exercise2';
+import Exercise1 from './demo/components/exercise1/Exercise1';
 
 function App() {
   return (
@@ -29,4 +30,4 @@ function App() {
   );
 }
 
-export default App;
+export default process.env.NODE_ENV === 'development' ? hot(module)(App) : App;
